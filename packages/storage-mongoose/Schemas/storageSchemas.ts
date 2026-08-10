e2568ff9c69f2834da1300830cfc8a8f13448a38
Ankit Methaniya <aa20494@gmail.com>
2026-08-11 02:36:51 +0530

    Implementing Mongoose

---
+++ packages/storage-mongoose/Schemas/storageSchemas.ts
@@ -0,0 +1,66 @@
+import { Schema, Model } from 'mongoose';
+import { AuditEvent, Actor, Target } from '@audit-framework/core';
+
+
+const actorSchema = new Schema<Actor>({
+    type: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    ip: {
+        type: String
+    },
+    userAgent: {
+        type: String
+    },
+    metadata: Schema.Types.Mixed,
+
+}, { _id: false })
+
+const targetSchema = new Schema<Target>({
+    type: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    metadata: Schema.Types.Mixed
+}, { _id: false })
+
+
+const eventSchema = new Schema<AuditEvent>({
+    id: {
+        type: String,
+    },
+    action: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ['success', 'failure']
+    },
+    actor: {
+        type: actorSchema,
+        required: true
+    },
+    target: {
+        type: targetSchema
+    },
+    changes: {
+        type: {
+            before: Schema.Types.Mixed,
+            after: Schema.Types.Mixed
+        }
+    },
+    metadata: {
+        type: Schema.Types.Mixed
+    }
+
+});
+
+export const EventModel = new Model('Events', eventSchema)
---
+++ packages/storage-mongoose/index.ts
@@ -0,0 +1,15 @@
+import { createConnection } from 'mongoose';
+import { StorageProvider } from '@audit-framework/core';
+import { EventModel } from './Schemas/storageSchemas.js';
+
+
+// interface for Mongoose options
+
+
+
+
+
+// Class to implement Storage Provider
+// export class MongooseStorage implements StorageProvider {
+
+// }
